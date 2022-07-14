bb66f57c7a684d39f479429d636a106b0c0e9409
anggifergian <anggyfergian56@gmail.com>
2022-07-14 17:58:53 +0700

    Change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname + '/build')));
 
