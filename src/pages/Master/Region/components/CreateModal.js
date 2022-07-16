7647cde9bd17b37029684557bdb31db8d34a0b4b
anggifergian <anggyfergian56@gmail.com>
2022-07-16 11:47:24 +0700

    Update Region

---
+++ src/pages/Master/Region/components/CreateModal.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Modal, Form, Input, Row, Col, Button } from 'antd'
+
+import { Title } from '../../../../containers'
+
+const CreateModal = ({ visible, onClose }) => {
+  const [form] = Form.useForm()
+
+  const handleSubmit = (values) => {
+    console.log('submitting...', values)
+  }
+
+  const closeModal = () => onClose()
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 6 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 18 },
+    }
+  }
+
+  return (
+    <Modal
+      title={<Title label="Form Region" />}
+      visible={visible}
+      onCancel={closeModal}
+      width={600}
+      footer={null}
+    >
+      <Form
+        {...formItemLayout}
+        form={form}
+        name='regionCreateModal'
+        onFinish={handleSubmit}
+        layout='horizontal'
+        autoComplete='off'
+      >
+        <Form.Item label="Region name" name="company_name">
+          <Input />
+        </Form.Item>
+
+        <Form.Item noStyle>
+          <Row align="middle" justify="end">
+            <Col>
+              <Button
+                id="btn-create-submit"
+                type="primary"
+                htmlType="submit"
+              >
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+export default CreateModal
--- src/pages/Master/Region/index.js
+++ src/pages/Master/Region/index.js
@@ -2,6 +2,8 @@ import { Button, Row } from 'antd'
 import React, { useState } from 'react'
 
 import { Title, BaseLayout } from '../../../containers'
+import SectionModal from './SectionModal'
+import SectionTable from './SectionTable'
 
 const Region = () => {
   const [state, setState] = useState({
@@ -35,7 +37,12 @@ const Region = () => {
       </Row>
 
       <div className='w-full rounded bg-white p-6'>
-        Table here
+        <SectionModal
+          modalType={state.typeModal}
+          visible={state.visible}
+          handleCloseModal={() => setState({ ...state, visible: false })}
+        />
+        <SectionTable />
       </div>
     </BaseLayout>
   )
