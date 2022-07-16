b43cf30423a62f41fd85db7c9a96a2f8be9cf3c5
anggifergian <anggyfergian56@gmail.com>
2022-07-16 15:33:38 +0700

    Add Promo pages

---
+++ src/pages/Promo/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Button, Row, Layout } from 'antd'
+
+import { Title, BaseLayout } from '../../containers'
+import SectionModal from './SectionModal'
+import SectionTable from './SectionTable'
+
+const { Content } = Layout
+
+const Promo = () => {
+  const [state, setState] = useState({
+    visible: false,
+    typeModal: ''
+  })
+
+  const handleShowModal = (typeModal = '') => {
+    setState({
+      ...state,
+      visible: !state.visible,
+      typeModal,
+    })
+  }
+
+  return (
+    <BaseLayout>
+      <Row
+        align="bottom"
+        justify="space-between"
+        style={{ marginBottom: 20 }}
+      >
+        <Title label="Promo" />
+
+        <Button
+          type='primary'
+          onClick={() => handleShowModal('create')}
+        >
+          Add Promo
+        </Button>
+      </Row>
+
+      <div className='w-full rounded bg-white p-6'>
+        <Content>
+          <SectionModal
+            modalType={state.typeModal}
+            visible={state.visible}
+            handleCloseModal={() => setState({ ...state, visible: false })}
+          />
+          <SectionTable />
+        </Content>
+      </div>
+    </BaseLayout>
+  )
+}
+
+export default Promo
