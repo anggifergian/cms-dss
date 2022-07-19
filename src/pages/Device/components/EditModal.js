421e739d810341105cee15210fd118570eb93c71
anggifergian <anggyfergian56@gmail.com>
2022-07-19 15:38:23 +0700

    Edit Device

--- src/pages/Device/components/EditModal.js
+++ src/pages/Device/components/EditModal.js
@@ -3,8 +3,10 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Modal, Form, Input, Row, Col, Button, Select } from 'antd'
 
 import { Title } from '../../../containers'
+import { requestCreateDevice } from '../../../redux/master/action'
 
 const EditModal = ({ visible, onClose, data }) => {
+  const dispatch = useDispatch()
   const Auth = useSelector(state => state.Auth)
   const Master = useSelector(state => state.Master)
   const [form] = Form.useForm()
@@ -18,6 +20,68 @@ const EditModal = ({ visible, onClose, data }) => {
     Master.reload && closeModal()
   }, [Master.reload, closeModal])
 
+  const fetchCreate = query => dispatch(requestCreateDevice(query))
+
+  const handleSubmit = (values) => {
+    const payload = {
+      endpoint: '/device/updateDevice',
+      data: {
+        ...values,
+        status: data['status'],
+        device_id: data['device_id'],
+        user_token: Auth.token,
+      }
+    }
+
+    fetchCreate(payload)
+  }
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 6 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 18 },
+    }
+  }
+
+  const EditForm = ({ data }) => {
+    return (
+      <Form
+        {...formItemLayout}
+        form={form}
+        name='branchCreateModal'
+        onFinish={handleSubmit}
+        layout='horizontal'
+        autoComplete='off'
+        initialValues={data}
+      >
+        <Form.Item
+          name="device_name"
+          label="Device name"
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item noStyle>
+          <Row align="middle" justify="end">
+            <Col>
+              <Button
+                id="btn-create-submit"
+                type="primary"
+                htmlType="submit"
+              >
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form.Item>
+      </Form>
+    )
+  }
+
   return (
     <Modal
       title={<Title label="Edit Device" />}
@@ -26,7 +90,7 @@ const EditModal = ({ visible, onClose, data }) => {
       width={600}
       footer={null}
     >
-      <div>EditModal</div>
+      <EditForm data={data} />
     </Modal>
   )
 }
